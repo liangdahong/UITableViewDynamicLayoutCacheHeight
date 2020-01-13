532c4904fc10186b4fa973841b24fb7526ec5add
梁大红 <ios@liangdahong.com>
2020-01-13 23:05:14 +0800

    add pod

---
+++ Pods/Target Support Files/UITableView-BMDynamicLayout/UITableView-BMDynamicLayout-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_UITableView_BMDynamicLayout : NSObject
+@end
+@implementation PodsDummy_UITableView_BMDynamicLayout
+@end
---
+++ Pods/Target Support Files/UITableView-BMDynamicLayout/UITableView-BMDynamicLayout-umbrella.h
@@ -0,0 +1,24 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#else
+#ifndef FOUNDATION_EXPORT
+#if defined(__cplusplus)
+#define FOUNDATION_EXPORT extern "C"
+#else
+#define FOUNDATION_EXPORT extern
+#endif
+#endif
+#endif
+
+#import "BMTableViewDynamicLayout.h"
+#import "UITableView+BMPrivate.h"
+#import "UITableViewCell+BMPrivate.h"
+#import "UITableViewDynamicLayoutMacro.h"
+#import "UITableViewHeaderFooterView+BMPrivate.h"
+#import "UITableView+BMDynamicLayout.h"
+#import "UITableViewCell+BMDynamicLayout.h"
+#import "UITableViewHeaderFooterView+BMDynamicLayout.h"
+
+FOUNDATION_EXPORT double UITableView_BMDynamicLayoutVersionNumber;
+FOUNDATION_EXPORT const unsigned char UITableView_BMDynamicLayoutVersionString[];
+
